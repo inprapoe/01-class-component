9205c8a4a61a7fb6601a705aba46ee43cd666f81
unknown <inprapoe@gmail.com>
2021-06-25 10:15:54 +0700

    update

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -3,29 +3,27 @@ import './User.css';
 
 import {IoLocationOutline} from 'react-icons/io5'
 
+
 class User extends Component {
 
     render() {
 
         return (
-            <li className="justify-between text-center max-w-xs mx-auto w-full space-y-5 bg-white shadow-lg rounded-lg overflow-hidden hover:shadow-xl transition duration-300 transform hover:scale-105 flex flex-col items-center">
-                <div className="flex space-y-5 flex-col items-center p-8">
-                    <p className="text-sm flex items-center space-x-4 font-semibold "><IoLocationOutline/>{this.props.user.address.city}</p>
-                    <div className="h-44 w-44 rounded-full grid place-items-center bg-white shadow-md">
+            <li className="flex flex-col items-center justify-between w-full max-w-xs mx-auto space-y-5 overflow-hidden text-center transition duration-300 transform bg-white rounded-lg shadow-lg hover:shadow-xl hover:scale-105">
+                <div className="flex flex-col items-center p-8 space-y-5">
+                    <p className="flex items-center space-x-4 text-sm font-semibold "><IoLocationOutline/>{this.props.user.address.city}</p>
+                    <div className="grid bg-white rounded-full shadow-md h-44 w-44 place-items-center">
                         <div className="user-pic">
                             <img src={this.props.picSrc} alt="" />
                         </div>
                     </div>
                     <div className="flex flex-col items-center">
-                        <h2 className="text-xl font-semibold mb-1 text-blue-700">{this.props.user.name}</h2>
+                        <h2 className="mb-1 text-xl font-semibold text-blue-700">{this.props.user.name}</h2>
                         <p className="text-xs">{this.props.user.email}</p>
                     </div>
-                    <p className="text-gray-500 text-sm"><i>"{this.props.user.company.catchPhrase}"</i></p>
+                    <p className="text-sm text-gray-500"><i>"{this.props.user.company.catchPhrase}"</i></p>
                 </div>
-                <div className="grid grid-cols-2 w-full border-t">
-                    <button className="h-12 w-full grid place-items-center bg-blue-600 text-white font-semibold text-sm hover:bg-blue-700 transition duration-300 tracking-widest">FOLLOW</button>
-                    <button className="h-12 w-full grid place-items-center bg-blue-100 text-blue-600 font-semibold text-sm hover:bg-blue-400 transition duration-300 tracking-widest hover:text-white">MESSAGE</button>
-                </div>
+                <button className="grid w-full h-12 mx-8 mb-8 text-sm font-semibold tracking-widest text-white transition duration-300 bg-blue-600 place-items-center hover:bg-blue-700">FOLLOW</button>
             </li>
         )
     }
